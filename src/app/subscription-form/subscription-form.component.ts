b125a2d270c1b050b896bc59396397f9ca4389e7
Abhishek Agrawal <abhishekacr@gmail.com>
2024-10-23 23:16:06 +0530

    sub form changes

--- src/app/subscription-form/subscription-form.component.ts
+++ src/app/subscription-form/subscription-form.component.ts
@@ -3,26 +3,34 @@ import { Sub } from '../models/sub';
 import { SubscribersService } from '../services/subscribers.service';
 
 
+
 @Component({
   selector: 'app-subscription-form',
   templateUrl: './subscription-form.component.html',
   styleUrl: './subscription-form.component.css'
 })
 export class SubscriptionFormComponent {
-
+  isEmailUsed: boolean = false;
+  isSubscribed: boolean = false;
   constructor(private subService: SubscribersService){}
 
 
   onSubmit(formValue:any){
-    console.log(formValue);
     const subData: Sub = {
       name: formValue.name,
       email: formValue.email
     }
-    // this.subService.addSub(subData);
-    this.subService.checkSub(subData.email).then(val=>{
-      console.log(val);
-      
+
+    this.subService.checkSub(subData.email).then(val=>{ 
+      if(val.empty){
+        this.subService.addSub(subData);
+        this.isEmailUsed = false;
+        this.isSubscribed = true;
+      }
+      else{
+        console.log('Email already exists!');
+        this.isEmailUsed = true;
+      }
     })
   }
 }
